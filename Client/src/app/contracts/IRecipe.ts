8a83d7698a19e684738f18d23a393df5de57f16e
matthofmarch <hofmarchermatthias@gmail.com>
2020-02-11 09:15:18 +0100

    Added contracts

---
+++ Client/src/app/contracts/IRecipe.ts
@@ -0,0 +1,13 @@
+import { IIngredient } from './IIngredient';
+import { IStep } from './IStep';
+
+export interface IRecipe {
+    id: number,
+    name: string,
+    ownerId: number,
+    ingredients: IIngredient[],
+    steps: IStep[];
+    estimatedTime: number;
+    estimatedCalories: number;
+
+}
