ed8d4f615fa29011297d1c4dc8df4ce879e88418
Maximilian K <m.kaindl0208@gmail.com>
2020-02-11 09:30:57 +0100

    fixed models

--- Client/src/app/contracts/IIngredient.ts
+++ Client/src/app/contracts/IIngredient.ts
@@ -1,5 +1,5 @@
 export interface IIngredient{
-    id:number,
-    name:string,
-    caloriesPerHundretGram: number,
+    id:number;
+    name:string;
+    caloriesPerHundretGram: number;
 }
--- Client/src/app/contracts/IRecipe.ts
+++ Client/src/app/contracts/IRecipe.ts
@@ -2,10 +2,10 @@ import { IIngredient } from './IIngredient';
 import { IStep } from './IStep';
 
 export interface IRecipe {
-    id: number,
-    name: string,
-    ownerId: number,
-    ingredients: IIngredient[],
+    id: number;
+    name: string;
+    ownerId: number;
+    ingredients: IIngredient[];
     steps: IStep[];
     estimatedTime: number;
     estimatedCalories: number;
