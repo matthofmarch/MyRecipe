1ba5b996cf24580575003c46b2384668c723eb61
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 11:48:22 +0100

    little changes

--- MyRecipeBackend/MyRecipeBackend/Controllers/GroupController.cs
+++ MyRecipeBackend/MyRecipeBackend/Controllers/GroupController.cs
@@ -27,7 +27,7 @@ namespace MyRecipeBackend.Controllers
             _userManger = userManger;
         }
 
-        [HttpPost("create")]
+        [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<GroupDto>> CreateGroup(string name)
@@ -52,65 +52,6 @@ namespace MyRecipeBackend.Controllers
             return CreatedAtAction(nameof(GetGroup), new GroupDto(group));
         }
 
-        [HttpGet("createInviteCode")]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<InviteCode>> CreateInviteCode()
-        {
-            var user = await _userManger.GetUserAsync(User);
-            if (user == null) return BadRequest("User not found");
-
-            var group = await _uow.Groups.GetGroupForUserAsync(user.Id);
-            if (group == null) return BadRequest("User not in Group");
-
-            var inviteCode = new InviteCode
-            {   //TODO generate as often until unique
-                Code = GenerateInviteCode(),
-                CreationDate = DateTime.Now
-            };
-
-            group.InviteCodes.Add(inviteCode);
-
-            try
-            {
-                await _uow.SaveChangesAsync();
-            }
-            catch (ValidationException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            return Ok(new InviteCodeDto(inviteCode));
-        }
-
-        [HttpGet("acceptInviteCode")]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> AcceptInvite(string inviteCode)
-        {
-            var user = await _userManger.GetUserAsync(User);
-            if (user == null) return BadRequest("User not found");
-
-            var group = await _uow.Groups.GetGroupForInviteCodeAsync(inviteCode.ToUpper());
-            if (group == null) return BadRequest("Invite Code does not belong to group");
-
-            // TODO Review
-            user.Group = group;
-            user.IsAdmin = false;
-            // group.Members.Add(user);
-            await _uow.InviteCodes.Delete(inviteCode.ToUpper());
-
-            try
-            {
-                await _uow.SaveChangesAsync();
-            }
-            catch (ValidationException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            return Ok();
-        }
 
         [HttpGet("getGroupForUser")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -127,14 +68,5 @@ namespace MyRecipeBackend.Controllers
             return new GroupDto(group);
         }
 
-        private static string GenerateInviteCode(int length = 6)
-        {
-            var rnd = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Range(1,length)
-                .Select(_ => chars[rnd.Next(chars.Length)])
-                .ToArray());
-
-        }
     }
 }
