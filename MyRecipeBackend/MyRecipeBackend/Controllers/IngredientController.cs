dc006df278adf9afc33059214ca4d2523c6318c6
Matthias Hofmarcher <hofmarchermatthias@gmail.com>
2020-12-07 12:00:33 +0100

    Added IngredientController

---
+++ MyRecipeBackend/MyRecipeBackend/Controllers/IngredientController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Contracts;
+using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyRecipeBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class IngredientController:ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+
+        public IngredientController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<Ingredient[]>> Get()
+        {
+            return await _uow.Ingredients.AllAsync();
+        }
+    }
+}
