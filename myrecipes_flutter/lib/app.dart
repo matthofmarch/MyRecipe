4e2ff23b31de812aa21b6aabce38a53bbb866067
Matthias Hofmarcher <hofmarchermatthias@gmail.com>
2021-01-07 11:44:13 +0100

    Added environment configs

--- myrecipes_flutter/lib/app.dart
+++ myrecipes_flutter/lib/app.dart
@@ -8,6 +8,7 @@ import 'package:group_repository/group_repository.dart';
 import 'package:http_interceptor/http_client_with_interceptor.dart';
 import 'package:ingredient_repository/ingredient_repository.dart';
 import 'package:meal_repository/meal_repository.dart';
+import 'package:myrecipes_flutter/env_config.dart';
 import 'package:myrecipes_flutter/interceptors/jsoncontent_interceptor.dart';
 import 'package:myrecipes_flutter/retry_policy/expired_token_retry_policy.dart';
 import 'package:myrecipes_flutter/splash/splash.dart';
@@ -17,18 +18,10 @@ import 'package:recipe_repository/recipe_repository.dart';
 import 'auth_guard/auth_guard.dart';
 import 'interceptors/bearer_interceptor.dart';
 
-const HOSTNAME = "vm133.htl-leonding.ac.at:5000";
-const PROTOCOL = "https";
-
-//const HOSTNAME = "10.0.2.2:5000";
-//const PROTOCOL = "http";
-
-
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final baseUrl = "$PROTOCOL://$HOSTNAME";
+    final baseUrl = EnvConfig.API_URL;
     final authRepository = AuthRepository(baseUrl);
 
     return RepositoryProvider<AuthRepository>(
@@ -38,7 +31,6 @@ class App extends StatelessWidget {
             BearerInterceptor(RepositoryProvider.of<AuthRepository>(context)),
             JsonContentInterceptor()
           ], retryPolicy: ExpiredTokenRetryPolicy(RepositoryProvider.of<AuthRepository>(context)));
-
           return MultiRepositoryProvider(
               providers: [
                 RepositoryProvider<MealRepository>(
