a69c34f8e40d7e92dda03aa831ac17e886170dc4
JulianRichtsfeld <31572115+JullianRichtsfeld@users.noreply.github.com>
2021-02-02 12:42:21 +0100

    changed app bar theme

--- myrecipes_flutter/lib/pages/meal_page/meal_view/meal_view.dart
+++ myrecipes_flutter/lib/pages/meal_page/meal_view/meal_view.dart
@@ -38,6 +38,7 @@ class MealView extends StatelessWidget {
               child: Row(
                 children: [
                   Icon(Icons.calendar_today_outlined),
+                  SizedBox(width: 5,),
                   Text(
                     DateFormat.yMEd().format(state.currentDate),
                   ),
