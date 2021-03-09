cf117e268811acd454d9228183d4874da21cda8f
Matthias Hofmarcher <hofmarchermatthias@gmail.com>
2021-03-09 19:43:11 +0100

    Integrated Invite Enabled DeveloperExceptionPage for non-Production environments

---
+++ myrecipes_flutter/lib/presentation/views/widgets/inviteView/invite_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:myrecipes_flutter/infrastructure/repositories/group_repository/group_repository.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class InviteView extends StatefulWidget {
+  @override
+  _InviteViewState createState() => _InviteViewState();
+}
+
+class _InviteViewState extends State<InviteView> {
+  String _inviteCode;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        initialData: "",
+        future: RepositoryProvider.of<GroupRepository>(context).getInviteCode(),
+        builder: (context, snapshot) {
+          return Column(mainAxisSize: MainAxisSize.min, children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text("Invite Code"), Icon(Icons.refresh_outlined)],
+            ),
+            Text(snapshot.data),
+            QrImage(
+              data: snapshot.data,
+              version: QrVersions.auto,
+              size: 200.0,
+            ),
+            Text("Code is one time use only!")
+          ]);
+        });
+  }
+}
