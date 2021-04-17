4656b3d5bda4fb481ee2f6049dfc982c8b02bbbd
JulianRichtsfeld <31572115+JullianRichtsfeld@users.noreply.github.com>
2021-04-17 13:40:22 +0200

    redesigned sign up

--- myrecipes_flutter/lib/presentation/views/screens/signup/signup.dart
+++ myrecipes_flutter/lib/presentation/views/screens/signup/signup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:myrecipes_flutter/presentation/view_models/screens/signup/signup_cubit.dart';
 
 class Signup extends StatelessWidget {
@@ -45,34 +46,75 @@ class Signup extends StatelessWidget {
     final _emailController = TextEditingController(text: state.previousEmail);
     final _passwordController = TextEditingController();
     return Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(children: [
-                TextFormField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(labelText: "Email"),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: "Password"),
-                ),
+        appBar: AppBar(
+          title: Text("Create your account"),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  SvgPicture.asset("assets/MyRecipe_Logo.svg",
+                      color: Theme.of(context).colorScheme.primary,
+                      width: 100,
+                      height: 100),
+                  Text(
+                    "MyRecipes",
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
+
+              Column(children: [
+                Card(
+                  elevation: 0,
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome,",
+                          style: Theme.of(context).textTheme.overline,
+                        ),
+                        Text(
+                          "Sign up!",
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(labelText: "Email"),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(labelText: "Password"),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
               ]),
-            ),
-            FlatButton(
-                onPressed: () => BlocProvider.of<SignupCubit>(context)
-                    .signup(_emailController.text, _passwordController.text),
-                child: Text("Sign Up")),
-          ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: 50,
+                child: OutlinedButton(
+                    onPressed: () => BlocProvider.of<SignupCubit>(context)
+                        .signup(_emailController.text, _passwordController.text),
+                    child: Text("Sign Up")),
+              )
+            ],
+          ),
         ));
   }
 }
